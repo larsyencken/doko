64a6004bbe3ab7d0abd6b3cfa541038d0497351d
Lars Yencken <lars@yencken.org>
2013-01-29 10:19:14 +1100

    Be more flexible with requirements in setup.py.
    Allow forward versions of dependencies.

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@ from setuptools import setup
 VERSION = '0.3.0'
 
 requires = [
-        'BeautifulSoup==3.2.1',
-        'requests==0.14.0',
-        'PyYAML==3.10',
+        'BeautifulSoup>=3.2.1',
+        'requests>=0.14.0',
+        'PyYAML>=3.10',
     ]
 
 corelocation_requires = [
-        'pyobjc==2.4',
-        'pyobjc-core==2.4',
-        'pyobjc-framework-CoreLocation==2.4',
+        'pyobjc>=2.4',
+        'pyobjc-core>=2.4',
+        'pyobjc-framework-CoreLocation>=2.4',
     ]
 
 setup(
